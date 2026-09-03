1de6276b4bf62dae20985e678686982cd15c409c
Juan López Rubio <j.lopez.r@gmail.com>
2026-09-03 09:55:34 +0200

    blinky has newer style

--- 3.fpga/blinky.v
+++ 3.fpga/blinky.v
@@ -1,33 +1,25 @@
-`ifdef VERILATOR
-/* verilator lint_off UNUSED */		// Not all of btn gets used, sigh
-module blinky(input i_clk, input [6:0] btn, output [7:0] o_led);
-/* verilator lint_on UNUSED */
-    wire i_clk;
-    wire [6:0] btn;
-    wire [7:0] o_led;
-`else
-module top(input clk_25mhz,
-           input [6:0] btn,
-           output [7:0] led,
-           output wifi_gpio0);
+`default_nettype none
 
-    wire i_clk;
+module top (
+    input  wire       clk_25mhz,
+    input  wire [6:0] btn,
+    output wire [7:0] led,
+    output wire       wifi_gpio0
+);
 
-    // Tie GPIO0, keep board from rebooting
+    // Evita que el ESP32 reinicie la placa.
     assign wifi_gpio0 = 1'b1;
-    assign i_clk= clk_25mhz;
-    reg [7:0] o_led;
-    assign led= o_led;
-`endif
 
-    localparam ctr_width = 32;
-    reg [ctr_width-1:0] ctr = 0;
+    reg [31:0] counter = 32'd0;
 
-    always @(posedge i_clk) begin
-               ctr <= ctr + 1;
-          o_led[7] <= 1;
-          o_led[6] <= btn[1];
-        o_led[5:0] <= ctr[23:18];
+    always @(posedge clk_25mhz) begin
+        counter <= counter + 1'b1;
     end
 
+    assign led[7]   = 1'b1;
+    assign led[6]   = btn[1];
+    assign led[5:0] = counter[23:18];
+
 endmodule
+
+`default_nettype wire
